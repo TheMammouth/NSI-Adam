3ea74bfd920e1c13844171831b6b629c7fcfa13a
TheMammouth <118438851+TheMammouth@users.noreply.github.com>
2022-11-17 11:56:55 +0100

    Ir

--- Iris.py
+++ Iris.py
@@ -7,12 +7,22 @@ Created on Thu Nov 17 11:02:43 2022
 
 import csv #Comma Separated Values
 from csv import DictReader 
-
+#converti les lignes en Dictionnaire python {}
 with open ("iris.csv") as f:
     table_fleur=[]
-    l_x=[]
-    l_y=[]
+    lon_fleur=[]
+    lar_fleur=[]
+    couleur=[]
     for ligne in DictReader(f):
-        ligne=dict(ligne)
-        print(ligne['longueur du sepale'])
-        l_xappend(float)
+        ligne=dict(ligne) #on converti les infos de la ligne 
+        lon_sep=float(ligne['longueur du sepale'])
+        lar_sep=float(ligne['largeur du sepale'])
+        if (ligne['espece']=='Iris-setosa'):
+            couleur.append([1,0,0])  
+        if (ligne['espece']=='Iris-versicolor'):
+            couleur.append([0,1,0])
+        if (ligne['espece']=='Iris-virginica'):
+            couleur.append([0,0,1])
+        lon_fleur.append(lon_sep)
+        lar_fleur.append(lar_sep)
+        
